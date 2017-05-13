5656167059c07e1c4ca2ccd410d65e2aa3685497
Jean Marc Haurand <jean.haurand@execart.de>
2017-05-13 05:37:00 +0200

    * added logo * modified container for fixed height * default font color

--- src/containers/SelectCar.js
+++ src/containers/SelectCar.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux';
 import { Container, Button } from '../components/ui';
+import { Dropdown } from 'react-toolbox';
 
 import styles from './selectCar.css'
 
@@ -8,6 +9,14 @@ import styles from './selectCar.css'
  * Select a car container.
  */
 class SelectCar extends Component {
+
+  state = {
+    dropdownValue: 1
+  };
+
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  };
 
   constructor() {
     super();
@@ -19,13 +28,42 @@ class SelectCar extends Component {
   componentWillReceiveProps(nextProps) {
   }
 
+
   render() {
 
+    const cars = [
+      { value: 1, label: 'VW Polo' },
+      { value: 2, label: 'VW Golf'},
+      { value: 3, label: 'VW Passat' },
+      { value: 4, label: 'VW Touran'}
+    ];
+
     return(
-      <Container className={styles.background}>
-        Select a car...<br/>
+      <Container width="fixed" className={styles.background}>
         <br/>
-        <Button primary raised href="/selectItems">Weiter</Button>
+        <img className={styles.logo} src="/assets/images/logo.png" alt="logo" />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Dropdown
+          label="Auto auswählen"
+          onChange={(value) => this.setState({value: value})}
+          source={cars}
+          value={this.state.value}
+        />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Button
+          primary
+          raised
+          onClick={() => this.context.router.push('/selectItems')}
+        >
+          Beladen
+        </Button>
       </Container>
     )
   }
