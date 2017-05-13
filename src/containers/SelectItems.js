3ec75e32b4a3e3a1a86d503bf0d007d8ac84c61f
Julian Tölle <julian.toelle97@gmail.com>
2017-05-13 20:31:38 +0200

    Incr on duplicate add to list

--- src/containers/SelectItems.js
+++ src/containers/SelectItems.js
@@ -27,37 +27,128 @@ class SelectItems extends Component {
       1: {
         picture: "http://www.ikea.com/de/de/images/products/jassa-couchtisch__0470185_PE612585_S4.JPG",
         name: "JASSA",
-        description: "Couchtisch"
+        description: "Couchtisch",
+        id: "903.456.63",
+        packages: [{ h: 12, w: 60, d: 65 }]
       },
       2: {
         picture: "http://www.ikea.com/de/de/images/products/byas-tv-bank-wei-__0144833_PE304277_S4.JPG",
         name: "BYÅS",
-        description: "TV-Bank"
+        description: "TV-Bank",
+        id: "802.277.97",
+        packages: [{ h: 7, w: 43, d: 171 }, { h: 5, w: 42, d: 171 }]
       },
       3: {
         picture: "http://www.ikea.com/de/de/images/products/pax-kleiderschrank-wei-__0300268_PE426268_S4.JPG",
         name: "Pax",
-        description: "Kleiderschrank mit Schiebetüren"
+        description: "Kleiderschrank mit Schiebetüren",
+        id: "102.373.56",
+        packages: [{ h: 5, w: 79, d: 235 }, { h: 7, w: 25, d: 235 }]
       },
       4: {
-        picture: "http://www.ikea.com/de/de/images/products/lack-beistelltisch-rot__0115088_PE268302_S4.JPG",
-        name: "Lack",
-        description: "Beistelltisch"
+        picture: "http://www.ikea.com/de/de/images/products/pax-kleiderschrank-wei-__0300268_PE426268_S4.JPG",
+        name: "Pax",
+        description: "Kleiderschrank mit Schiebetüren",
+        id: "202.145.71",
+        packages: [{ h: 7, w: 60, d: 245 }]
       },
       5: {
-        picture: "http://www.ikea.com/de/de/images/products/klippan-er-sofa-grau__0239990_PE379591_S4.JPG",
-        name: "Kippan Sofa",
-        description: "2er Sofa"
+        picture: "http://www.ikea.com/de/de/images/products/pax-kleiderschrank-wei-__0300268_PE426268_S4.JPG",
+        name: "Pax",
+        description: "Kleiderschrank mit Schiebetüren",
+        id: "302.632.45",
+        packages: [{ h: 4, w: 8, d: 67 }]
       },
       6: {
-        picture: "http://www.ikea.com/de/de/images/products/kallax-regal-wei-__0243994_PE383246_S4.JPG",
-        name: "Kallax",
-        description: "Regal"
+        picture: "http://www.ikea.com/de/de/images/products/pax-kleiderschrank-wei-__0300268_PE426268_S4.JPG",
+        name: "Pax",
+        description: "Kleiderschrank mit Schiebetüren",
+        id: "402.568.95",
+        packages: [{ h: 4, w: 6, d: 73 }]
       },
       7: {
+        picture: "http://www.ikea.com/de/de/images/products/pax-kleiderschrank-wei-__0300268_PE426268_S4.JPG",
+        name: "Pax",
+        description: "Kleiderschrank mit Schiebetüren",
+        id: "902.779.61",
+        packages: [{ h: 2, w: 59, d: 71 }]
+      },
+      8: {
+        picture: "http://www.ikea.com/de/de/images/products/billy-bucherregal-wei-__0252367_PE391149_S4.JPG",
+        name: "Billy",
+        description: "Bücherregal",
+        id: "002.638.50",
+        packages: [{ h: 13, w: 29, d: 205 }]
+      },
+      9: {
+        picture: "http://www.ikea.com/de/de/images/products/lack-beistelltisch-rot__0115088_PE268302_S4.JPG",
+        name: "Lack",
+        description: "Beistelltisch",
+        id: "801.937.35",
+        packages: [{ h: 5, w: 55, d: 75 }]
+      },
+      10: {
+        picture: "http://www.ikea.com/de/de/images/products/klippan-er-sofa-grau__0239990_PE379591_S4.JPG",
+        name: "Klippan Sofa",
+        description: "2er Sofa",
+        id: "100.722.56",
+        packages: [{ h: 40, w: 91, d: 182 }]
+      },
+      11: {
+        picture: "http://www.ikea.com/de/de/images/products/klippan-er-sofa-grau__0239990_PE379591_S4.JPG",
+        name: "Klippan Sofa",
+        description: "2er Sofa",
+        id: "202.788.55",
+        packages: [{ h: 8, w: 28, d: 37 }]
+      },
+      12: {
+        picture: "http://www.ikea.com/de/de/images/products/poang-sessel-grau__0497180_PE629007_S4.JPG",
+        name: "Poäng Schwingsessel",
+        description: "Sessel",
+        id: "500.547.26",
+        packages: [{ h: 7, w: 68, d: 73 }]
+      },
+      13: {
+        picture: "http://www.ikea.com/de/de/images/products/poang-sessel-grau__0497180_PE629007_S4.JPG",
+        name: "Poäng Schwingsessel",
+        description: "Sessel",
+        id: "703.624.70",
+        packages: [{ h: 15, w: 58, d: 70 }]
+      },
+      14: {
+        picture: "http://www.ikea.com/de/de/images/products/kallax-regal-wei-__0243994_PE383246_S4.JPG",
+        name: "Kallax",
+        description: "Regal",
+        id: "802.758.87",
+        packages: [{ h: 17, w: 41, d: 152 }]
+      },
+      15: {
+        picture: "http://www.ikea.com/de/de/images/products/ektorp-er-sofa-grau__0386827_PE559169_S4.JPG",
+        name: "Ektorp Sofa",
+        description: "3er Sofa",
+        id: "201.850.31",
+        packages: [{ h: 45, w: 88, d: 201 }]
+      },
+      16: {
         picture: "http://www.ikea.com/de/de/images/products/leifarne-stuhl-blau__0376674_PE553887_S4.JPG",
         name: "Leifarne Stuhl",
-        description: "Stuhl für Esszimmer"
+        description: "Stuhl für Esszimmer",
+        id: "102.919.18",
+        packages: [{ h: 43, w: 45, d: 50 }]
+      },
+      17: {
+        picture: "http://www.ikea.com/de/de/images/products/leifarne-stuhl-blau__0376674_PE553887_S4.JPG",
+        name: "Leifarne Stuhl",
+        description: "Stuhl für Esszimmer",
+        id: "002.879.74",
+        packages: [{ h: 6, w: 36, d: 84 }]
+      },
+      18: {
+        picture: "http://www.ikea.com/de/de/images/products/strandmon-ohrensessel-braun__0534682_PE649222_S4.JPG",
+        name: "Strandmon",
+        description: "Sessel",
+        id: "303.223.39",
+        packages: [{ h: 83, w: 92, d: 93 }]
       }
     };
 
@@ -131,11 +222,16 @@ class SelectItems extends Component {
    * Add product by product id.
    */
   addProduct(productId) {
-    console.log(productId);
-    this.inCarList.push({
-      productId: productId,
-      count: 1
-    });
+    const prevElement = this.inCarList.find(item => productId === item.productId);
+
+    if(prevElement) {
+      prevElement.count++;
+    } else {
+      this.inCarList.push({
+        productId: productId,
+        count: 1
+      });
+    }
     this.totalCount++;
     this.forceUpdate();
   }
